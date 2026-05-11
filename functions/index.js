cf437841354329df8ad9724e6b3dfc25a4260274
crownmetrics <crownmetricscom@gmail.com>
2026-05-11 04:15:23 +0200

    firebase env newline fix

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,12 @@ const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
+serviceAccount.private_key =
+serviceAccount.private_key.replace(
+  /\\n/g,
+  "\n"
+);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL:
