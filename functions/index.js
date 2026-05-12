d31e6b7722c85d6a8d2c96ca56d3c4f99e5e9fec
crownmetricscom-spec <crownmetricscom@gmail.com>
2026-05-12 08:41:04 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -67,16 +67,7 @@ currentKeyIndex + 1
 
 let currentKeyIndex = 0;
 
-const API_KEYS = [
 
-process.env.YOUTUBE_API_KEY_1,
-process.env.YOUTUBE_API_KEY_2,
-process.env.YOUTUBE_API_KEY_3,
-process.env.YOUTUBE_API_KEY_4,
-process.env.YOUTUBE_API_KEY_5,
-process.env.YOUTUBE_API_KEY_6
-
-];
 
 let COUNTRIES = [
 
