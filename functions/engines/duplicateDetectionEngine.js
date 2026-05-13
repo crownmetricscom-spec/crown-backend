6d7f4a6fc811055f9b7d4b9d7e98bca276ea6741
crownmetricscom-spec <crownmetricscom@gmail.com>
2026-05-13 02:57:30 +0200

    Update duplicateDetectionEngine.js

--- functions/engines/duplicateDetectionEngine.js
+++ functions/engines/duplicateDetectionEngine.js
@@ -1,1 +1,60 @@
+function normalizeTitle(title) {
 
+return title
+.toLowerCase()
+.replace(/[^\w\s]/g, "")
+.replace(/\s+/g, " ")
+.trim();
+
+}
+
+
+
+function detectDuplicateVideos(videos) {
+
+const titleMap = {};
+
+const duplicateIds = [];
+
+for (const video of videos) {
+
+const normalized =
+normalizeTitle(video.title);
+
+if (!titleMap[normalized]) {
+
+titleMap[normalized] = [];
+
+}
+
+titleMap[normalized].push(video.id);
+
+}
+
+
+
+for (const key in titleMap) {
+
+if (titleMap[key].length > 1) {
+
+duplicateIds.push(
+...titleMap[key]
+);
+
+}
+
+}
+
+
+
+return duplicateIds;
+
+}
+
+
+
+module.exports = {
+
+detectDuplicateVideos
+
+};
