8c374b801aa3ea5f0cbdf33325427748a92ef740
crownmetricscom-spec <crownmetricscom@gmail.com>
2026-05-13 03:08:57 +0200

    Update momentumEngine.js

--- functions/engines/momentumEngine.js
+++ functions/engines/momentumEngine.js
@@ -1,1 +1,49 @@
+function calculateMomentum({
 
+  velocity,
+  rankChange,
+  ageHours
+
+}) {
+
+  let momentum = 0;
+
+  // VELOCITY POWER
+
+  momentum += Math.min(
+    velocity / 1000,
+    50
+  );
+
+  // RANK MOVEMENT
+
+  momentum += rankChange * 8;
+
+  // FRESHNESS BONUS
+
+  if (ageHours <= 24) {
+
+    momentum += 20;
+
+  }
+
+  else if (ageHours <= 72) {
+
+    momentum += 10;
+
+  }
+
+  // FLOOR
+
+  momentum = Math.max(
+    Math.floor(momentum),
+    0
+  );
+
+  return momentum;
+
+}
+
+module.exports = {
+  calculateMomentum
+};
