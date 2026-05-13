92cb68deb232f8a8d87b7f80b0c905bdd36b8a7c
crownmetricscom-spec <crownmetricscom@gmail.com>
2026-05-13 02:29:22 +0200

    Update scoreEngine.js

--- functions/engines/scoreEngine.js
+++ functions/engines/scoreEngine.js
@@ -1,1 +1,129 @@
+function calculateScore({
+views,
+likes,
+comments,
+ageHours
+}) {
+
+let score = Math.floor(
+
+```
+(
+
+  (
+    likes +
+    (comments * 2)
+  )
+
+  /
+
+  (views || 1)
+
+)
+
+*
+
+10000
+
+*
+
+(
+
+  ageHours < 24
+    ? 1.5
+    : ageHours < 72
+    ? 1.2
+    : 1
+
+)
+```
+
+);
+
+return score;
+
+}
+
+function calculateStatus({
+score,
+velocity,
+rankChange,
+index
+}) {
+
+let status = "stable";
+
+if (
+score >= 900 &&
+velocity >= 30000 &&
+rankChange >= 2
+) {
+
+```
+status = "viral";
+```
+
+}
+
+else if (
+score >= 700 &&
+velocity >= 15000
+) {
+
+```
+status = "hot";
+```
+
+}
+
+else if (
+rankChange >= 3
+) {
+
+```
+status = "rising";
+```
+
+}
+
+else if (
+rankChange <= -3
+) {
+
+```
+status = "falling";
+```
+
+}
+
+else if (
+velocity >= 40000 &&
+index >= 15
+) {
+
+```
+status = "hidden_gem";
+```
+
+}
+
+else if (
+index === 0 &&
+score >= 800
+) {
+
+```
+status = "champion";
+```
+
+}
+
+return status;
+
+}
+
+module.exports = {
+calculateScore,
+calculateStatus
+};
 
